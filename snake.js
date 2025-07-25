83e9fbe8bfaf6260c172f3bad8931211f2d43537
megapepper <fenya1009@gmail.com>
2025-07-25 13:15:28 +0300

    more features

--- snake.js
+++ snake.js
@@ -6,29 +6,32 @@ function print(obj) {
     console.log(JSON.stringify(obj))
 }
 
-document.onkeydown = directions;
+document.onkeydown = keyBar; 
 
+let timeMove = 400;
+const delay = 50;
 const cell_size = 50;
-const time_move = 100;
-
 const LEFT = 37;
 const UP = 38;
 const RIGHT = 39;
 const DOWN = 40;
+const SPACE = 32;
 const direction_codes = { LEFT: 1, UP: 2, RIGHT: 3, DOWN: 4 };
-const snake_part = { 'head': 1, 'snake': 2, 'tail': 3 };
-
 const w = window.innerWidth;
 const h = window.innerHeight;
 
+let prevKeyCode = RIGHT;
 let rows = Math.floor(h / (cell_size));
 let cols = Math.floor(w / cell_size);
 let matrix = Array.from({ length: rows }, () => new Array(cols).fill(0));
 let cells;
 let snake;
-let food;
+let food = [];
 let foodEaten = false;
-let cntFood = 0;
+let cntFoodEaten = 0;
+let initCntFood = 3;
+let immortal = false;
+let paused = false;
 
 const margin_side = Math.floor((w - cell_size * cols) / 2);
 const margin_top = Math.floor((h - cell_size * rows) / 2);
@@ -43,12 +46,17 @@ function initSnake() {
     tail_col = col_mid - 1;
 
     snake = [[head_row, head_col], [row_mid, col_mid], [tail_row, tail_col]];
-    snake1d = snake.map(function (x) { return x[0] * cols + x[1]; });
 
     cells = document.getElementsByClassName('cell');
     cells[head_row * cols + head_col].classList.add('snake-head', 'snake');
     cells[head_row * cols + col_mid].classList.add('snake');
     cells[head_row * cols + tail_col].classList.add('snake-tail', 'snake');
+}
+
+function initFood() {
+    for (let i = 0; i < initCntFood; i++) {
+        generateFood();
+    }
 }
 
 function initField() {
@@ -69,25 +77,44 @@ function initField() {
         }
     }
     initSnake();
-    generateFood();
+    initFood();
+    startInterval();
 }
 
-function drawSnake(head_row, head_col, tail_row, tail_col) {
-    head_old = cells[head_row * cols + head_col];
-    head_old.classList.remove('snake-head');
+function drawSnake(head_row = 0, head_col = 0, tail_row = 0, tail_col = 0, cutPos = -1) {
+    if (cutPos == -1) {
+        head_old = cells[head_row * cols + head_col];
+        head_old.classList.remove('snake-head');
 
-    tail_old = cells[tail_row * cols + tail_col];
-    tail_old.classList.remove('snake-tail', 'snake');
+        tail_old = cells[tail_row * cols + tail_col];
+        tail_old.classList.remove('snake-tail', 'snake');
 
-    [new_head_row, new_head_col] = snake[0];
-    cells[new_head_row * cols + new_head_col].classList.add('snake-head', 'snake');
+        [new_head_row, new_head_col] = snake[0];
+        cells[new_head_row * cols + new_head_col].classList.add('snake-head', 'snake');
 
-    [new_tail_row, new_tail_col] = snake.at(-1);
-    cells[new_tail_row * cols + new_tail_col].classList.add('snake-tail', 'snake');
+        [new_tail_row, new_tail_col] = snake.at(-1);
+        cells[new_tail_row * cols + new_tail_col].classList.add('snake-tail', 'snake');
+    }
+    else {
+        [new_tail_row, new_tail_col] = snake[cutPos - 1];
+        cells[new_tail_row * cols + new_tail_col].classList.add('snake-tail');
+        for (let i = cutPos; i < snake.length; i++) {
+            [row, col] = snake[i];
+            cells[row * cols + col].classList.remove('snake-tail', 'snake');
+        }
+        [new_head_row, new_head_col] = snake[0];
+        cells[new_head_row * cols + new_head_col].classList.add('snake');
+    }
 }
 
-function drawFood(pos) {
-    cells[pos].classList.add('food');
+function drawFood(pos, flag = true) {
+    if (flag) {
+        cells[pos].classList.add('food');
+    }
+    else {
+        cells[pos].classList.remove('food');
+    }
+
 }
 
 function move_right() {
@@ -156,81 +183,119 @@ function move_down() {
 
 function generateFood() {
     let snake1d = snake.map(function (x) { return x[0] * cols + x[1]; });
-    pos = Math.floor(Math.random() * (rows * cols - snake.length));
+    pos = Math.floor(Math.random() * (rows * cols - snake.length - initCntFood));
     cnt = 0;
     for (i = 0; i < cells.length; i++) {
         if (snake1d.includes(i)) continue;
+        if (food.includes(i)) continue;
         if (cnt == pos) {
             drawFood(i);
             break;
         }
         cnt++;
     }
-    food = i;
+    food.push(i);
 }
 
 function deleteFood(pos) {
-    cells[pos].classList.remove('food');
+    var index = food.indexOf(pos);
+    food.splice(index, 1);
+    drawFood(pos, false);
+}
+
+function increaseSpeed() {
+    if (cntFoodEaten == 2) {
+        timeMove = timeMove / 2;
+    }
 }
 
 function checkFoodEaten() {
     let snake1d = snake.map(function (x) { return x[0] * cols + x[1]; });
     head_pos = snake1d[0];
-    if (head_pos == food) {
-        deleteFood(food);
+    if (food.includes(head_pos)) {
+        deleteFood(head_pos);
         generateFood();
         foodEaten = true;
-        cntFood++;
+        cntFoodEaten++;
+        increaseSpeed();
     }
+}
+
+function cutSnake(pos) {
+    let snake1d = snake.map(function (x) { return x[0] * cols + x[1]; });
+    var index = snake1d.slice(1).indexOf(pos);
+    drawSnake(null, null, null, null, index);
+    snake.length = index;
 }
 
 function checkLoss() {
     let snake1d = snake.map(function (x) { return x[0] * cols + x[1]; });
     head_pos = snake1d[0];
     if (snake1d.slice(1).includes(head_pos)) {
-        console.log('LOSS');
-        clearInterval(intervalId);
-        showLoss();
+        if (!immortal) {
+            clearInterval(intervalId);
+            showLoss();
+        }
+        else {
+            cutSnake(head_pos);
+        }
+
     }
 }
 
 function showLoss() {
-    alert(`You lost! Your earned ${cntFood} points. Let's play again?`);
+    alert(`You lost! Your earned ${cntFoodEaten} points. Let's play again?`);
     location.reload();
 }
 
 let keyCode = null;
-let prevKeyCode = RIGHT;
+let timeDelay = 0;
+let intervalId;
 
-let intervalId = setInterval(() => {
-    if (keyCode == null) {
-        return
-    }
-    if (keyCode == RIGHT) {
-        move_right();
-    }
-    if (keyCode == LEFT) {
-        move_left();
-    }
-    if (keyCode == UP) {
-        move_up();
-    }
-    if (keyCode == DOWN) {
-        move_down();
-    }
-    checkFoodEaten();
-    checkLoss();
-    prevKeyCode = keyCode;
-}, time_move);
+function startInterval() {
+        intervalId = setInterval(() => {
+        timeDelay += delay;
+        if (timeDelay >= timeMove) {
+            timeDelay = 0;
+            if (keyCode == null) {
+                return
+            }
+            if (keyCode == RIGHT) {
+                move_right();
+            }
+            if (keyCode == LEFT) {
+                move_left();
+            }
+            if (keyCode == UP) {
+                move_up();
+            }
+            if (keyCode == DOWN) {
+                move_down();
+            }
+            checkFoodEaten();
+            checkLoss();
+            prevKeyCode = keyCode;
+        }
+    }, delay);
+}
 
 
-function directions(e) {
+function keyBar(e) {
     e = e || window.Event;
     if ([RIGHT, LEFT, UP, DOWN].includes(e.keyCode)) {
         if (e.keyCode == RIGHT && prevKeyCode != LEFT || e.keyCode == LEFT && prevKeyCode != RIGHT ||
             e.keyCode == UP && prevKeyCode != DOWN || e.keyCode == DOWN && prevKeyCode != UP) {
             keyCode = e.keyCode;
         }
-
+    }
+    if (e.keyCode == SPACE) {
+        if (!paused) {
+            clearInterval(intervalId);
+            paused = true;
+        }
+        else {
+            startInterval();
+            paused = false;
+        }
     }
 }
